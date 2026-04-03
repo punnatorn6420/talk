2fd412ea609c065f88724667cee1d9216352fd16
punnatorn <punnatornzzzzz@gmail.com>
2026-04-03 11:21:01 +0700

    feat: update dashboard layout with button component and grid structure

--- apps/frontend/src/app/pages/dashboard/dashboard.component.ts
+++ apps/frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SubscriptionDestroyer } from '../../shared/core/helper/SubscriptionDestroyer.helper';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
+  imports: [Button],
 })
 export class DashboardComponent extends SubscriptionDestroyer {
   constructor() {
